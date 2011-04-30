50ff0d85545da2624439a43177524892410a771d
philomory <philomory@localhost>
2011-04-29 20:53:48 -1000

    Completed first draft dungeon layout algorithm.

--- test.rb
+++ test.rb
@@ -3,9 +3,9 @@ def gen_layout
   entry
   pre_item
   item
-  #post_item
-  #magic_key
-  #goal
+  post_item
+  magic_key
+  goal
 end
 
 def setup
@@ -13,15 +13,18 @@ def setup
   @stages_post_item_range = (2..4)
   @rooms_per_normal_stage = (2..4)
   @rooms_added_for_item_step = (1..3)
+  @rooms_added_for_magic_key_step = (0..2)
     
   @dungeon_map = {}
   @stages = []
   @all_rooms = []
   @latest_room = nil
   @next_switch_id = 0
+  @current_step = nil
 end
 
 def entry
+  @current_step = :entry
   room = {
     x: 0,
     y: 0,
@@ -52,6 +55,16 @@ def add_room(room)
   @latest_room = room
 end
 
+def step_room(*args)
+  case @current_step
+  when :pre_item then pre_item_room(*args)
+  when :item then post_item_room(*args)
+  when :post_item then post_item_room(*args)
+  else
+    raise "Blarg! Bad room step: #{@current_step}"
+  end
+end
+
 def pick_a_room(options = {})
   options = {open_only: true, this_stage: true, untyped: false}.update(options)
   base_set = options[:this_stage] ? @stages.last : @all_rooms
@@ -61,6 +74,7 @@ def pick_a_room(options = {})
 end
 
 def pre_item
+  @current_step = :pre_item
   number_of_stages = @stages_pre_item_range.to_a.sample
   number_of_stages.times do
     pre_item_stage
@@ -89,7 +103,7 @@ def pre_item_room(room_to_branch,door_type=:normal)
   room = {
     x: x,
     y: y,
-    step: :pre_item,
+    step: @current_step,
     available_edges: [:n,:s,:e,:w],
     connected_rooms: {}
   }
@@ -98,17 +112,20 @@ def pre_item_room(room_to_branch,door_type=:normal)
 end
 
 def item
+  @current_step = :item
   @latest_room[:step] = :item
   @latest_room[:type] = :item
   
+  type_of_obstacle = [:item_key_lock, :item_switch_lock].sample
   number_of_rooms = @rooms_added_for_item_step.to_a.sample
   number_of_rooms.times do
     room_to_branch = pick_a_room(this_stage: false)
-    item_stage_room(room_to_branch)
+    item_step_room(room_to_branch)
   end
+  branch_obstacle(type_of_obstacle)
 end
 
-def item_stage_room(room_to_branch)
+def item_step_room(room_to_branch)
   direction = room_to_branch[:available_edges].sample
   x,y = coords_in_direction(room_to_branch,direction)
   room = {
@@ -122,12 +139,76 @@ def item_stage_room(room_to_branch)
   connect_rooms(room_to_branch,room,direction,:item_blocked)
 end
 
+def post_item
+  @current_step = :post_item
+  
+  number_of_stages = @stages_post_item_range.to_a.sample
+  number_of_stages.times do
+    post_item_stage
+  end
+end
+
+def post_item_stage
+  type_of_obstacle = [:key_lock, :switch_lock, :item_key_lock, :item_switch_lock].sample
+  number_of_rooms = @rooms_per_normal_stage.to_a.sample - 1
+  number_of_rooms.times do
+    room_to_branch = pick_a_room
+    if room_to_branch
+      post_item_room(room_to_branch)
+    else
+      type_of_obstacle = case type_of_obstacle
+      when :key_lock then :switch_lock
+      when :item_key_lock then :item_switch_lock
+      end
+      puts "Boo! No rooms in this stage that can be branched! (Post-item)"
+      break
+    end
+  end
+  branch_obstacle(type_of_obstacle)
+end
+
+def post_item_room(*args)
+  pre_item_room(*args)
+end
+
+def magic_key
+  @current_step = :magic_key
+  @latest_room[:step] = :magic_key
+  
+  number_of_rooms = @rooms_added_for_magic_key_step.to_a.sample
+  number_of_rooms.times do
+    room_to_branch = pick_a_room
+    door_type = [:normal, :item_blocked].sample
+    post_item_room(room_to_branch,door_type)
+  end
+  @latest_room[:type] = :magic_key
+end
+
+def goal
+  @current_step = :goal
+  start_new_stage
+  room_to_branch = pick_a_room(untyped: true, this_stage: false)
+  direction = room_to_branch[:available_edges].sample
+  x,y = coords_in_direction(room_to_branch,direction)
+  room = {
+    x: x,
+    y: y,
+    step: :goal,
+    available_edges: [],
+    connected_rooms: {}
+  }
+  add_room(room)
+  connect_rooms(room_to_branch,room,direction,:magic_door)
+end
+
 def branch_obstacle(type)
   case type
   when :key_lock then branch_key_lock
   when :switch_lock then branch_switch_lock
+  when :item_key_lock then branch_item_key_lock
+  when :item_switch_lock then branch_item_switch_lock
   else
-    raise "That's not a valid obstacle type!"
+    raise "That's not a valid obstacle type! (#{type})"
   end
 end
 
@@ -137,7 +218,7 @@ def branch_key_lock
   
   room_for_key[:type] = :key
   start_new_stage
-  pre_item_room(room_to_branch,:key_locked)
+  step_room(room_to_branch,:key_locked)
 end
 
 def branch_switch_lock
@@ -148,7 +229,29 @@ def branch_switch_lock
   room_for_switch[:switch_id] = @next_switch_id
   
   start_new_stage
-  pre_item_room(room_to_branch,:switch_locked)
+  step_room(room_to_branch,:switch_locked)
+  @next_switch_id += 1
+end
+
+def branch_item_key_lock
+  room_to_branch = pick_a_room
+  room_for_key = pick_a_room(open_only: false, this_stage: false, untyped: true)
+  
+  room_for_key[:type] = :item_blocked_key
+  start_new_stage
+  step_room(room_to_branch,:key_locked)
+end
+
+def branch_item_switch_lock
+  room_to_branch = pick_a_room(this_stage: false)
+  room_for_switch = pick_a_room(open_only: false, untyped: true)
+  
+  room_for_switch[:type] = :item_blocked_switch
+  room_for_switch[:switch_id] = @next_switch_id
+  
+  start_new_stage
+  step_room(room_to_branch,:switch_locked)
+  @next_switch_id += 1
 end
 
 def connect_rooms(branching_room,new_room,direction,door_type=:normal)
@@ -196,3 +299,4 @@ require 'pp'
 gen_layout
 
 pp @dungeon_map
+puts @dungeon_map.size
