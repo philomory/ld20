6b9c8688f29790e8f39bc7ab9f8b31ce0ef4991f
philomory <philomory@localhost>
2011-04-30 02:21:44 -1000

    Partial room wall graphic added. Based on perspective from original LoZ

--- app/TestState.rb
+++ app/TestState.rb
@@ -1,7 +1,7 @@
 module LD20
   class TestState < Chingu::GameState
     def setup
-      @grid = Grid.new(FullMapTilesWide, FullMapTilesHigh)
+      @grid = Grid.new(FullMapTilesWide, FullMapTilesHigh-1)
       @grid.each_with_coords {|i,x,y| @grid[x,y] = [:floor1, :floor2].sample }
       @images = {
         floor1: Image["floor1.png"],
@@ -13,9 +13,10 @@ module LD20
       @grid.each_with_coords do |type,x,y|
         i = @images[type]
         x_pos = x * TileWidth
-        y_pos = y * TileHeight
+        y_pos = y * TileHeight + 32
         i.draw(x_pos,y_pos,0)
       end
+      Image["border.png"].draw(0,32,100)
     end
   end
 end
