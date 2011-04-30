8535ab2c16dc5ce8d6cce9ba91aebe3d66145b91
philomory <philomory@localhost>
2011-04-30 00:55:40 -1000

    Beginning to bring in basecode, starting with the Grid class.

---
+++ app/Constants.rb
@@ -0,0 +1,10 @@
+module LD20
+  WindowWidth = 640
+  WindowHeight = 480
+  
+  TileWidth = 32
+  TileHeight = 32
+  
+  FullMapTilesWide = 20
+  FullMapTilesHigh = 15
+end
---
+++ app/Grid.rb
@@ -0,0 +1,47 @@
+module LD20
+  class Grid
+    include Enumerable
+    attr_reader :width, :height
+    
+    def initialize(width,height)
+      @width, @height = width, height
+      @spaces = Array.new(@width) {Array.new(@height)}
+    end
+    
+    def [](x,y)
+      if (0...@width).include?(x) and (0...@height).include?(y)
+        return @spaces[x][y]
+      else
+        return :out_of_bounds
+      end
+    end
+    
+    def []=(x,y,val)
+      if (0...@width).include?(x) and (0...@height).include?(y)
+        @spaces[x][y] = val
+      end
+    end
+    
+    def each
+      @spaces.each do |col|
+        col.each do |obj|
+          yield obj
+        end
+      end
+    end
+    
+    def each_with_coords
+      if block_given?
+        @spaces.each_with_index do |col,x|
+          col.each_with_index do |obj,y|
+            yield obj, x, y
+          end
+        end
+      else
+        return Enumerator.new(self,:each_with_coords)
+      end
+    end
+    
+    
+  end
+end
